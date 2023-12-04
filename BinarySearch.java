b1e0936c4aa59f3fb086308c18245e4622bfb6e1
Pavan19047 <118459281+Pavan19047@users.noreply.github.com>
2023-12-04 18:51:25 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,49 +1,46 @@
-import java.io.*;
+class BinarySearch {
  
 
-class BinarySearch {
+    // Returns index of x if it is present in arr[l.. // r], else return -1
 
-   
-
-    // Returns index of x if it is present in arr[].
-
-    int binarySearch(int arr[], int x)
+    int binarySearch(int arr[], int l, int r, int x)
 
     {
 
-        int l = 0, r = arr.length - 1;
+        if (r >= l) {
 
-        while (l <= r) {
-
-            int m = l + (r - l) / 2;
+            int mid = l + (r - l) / 2;
  
 
-            // Check if x is present at mid
+            // If the element is present        at the
+           // middle itself
 
-            if (arr[m] == x)
+            if (arr[mid] == x)
 
-                return m;
+                return mid;
  
 
-            // If x greater, ignore left half
+            // If element is smaller than mid, then
 
-            if (arr[m] < x)
+            // it can only be present in left subarray
 
-                l = m + 1;
+            if (arr[mid] > x)
+
+                return binarySearch(arr, l, mid - 1, x);
  
 
-            // If x is smaller, ignore right half
+            // Else the element can only be present
 
-            else
+            // in right subarray
 
-                r = m - 1;
+            return binarySearch(arr, mid + 1, r, x);
 
         }
  
 
-        // If we reach here, then element was
+        // We reach here when element is not present
 
-        // not present
+        // in array
 
         return -1;
 
@@ -64,7 +61,7 @@ class BinarySearch {
 
         int x = 10;
 
-        int result = ob.binarySearch(arr, x);
+        int result = ob.binarySearch(arr, 0, n - 1, x);
 
         if (result == -1)
 
@@ -74,9 +71,9 @@ class BinarySearch {
 
         else
 
-            System.out.println("Element is present at "
+            System.out.println(
 
-                               + "index " + result);
+                "Element is present at index " + result);
 
     }
 }
